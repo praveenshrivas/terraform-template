0731b7a25f1ecbdbfb532b615eacbe42a32322b7
praveenshrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-01-20 15:39:32 +0530

    Update hcl-basics-first-resource.tf
    added some description

--- hcl-basics-first-resource.tf
+++ hcl-basics-first-resource.tf
@@ -3,3 +3,5 @@ resource "local_sensitive_file" "games" {
   content  = "FIFA 21"
   sensitive_content = "FIFA 21"
 }
+# if you want that the content should not be printed on the screen 
+# then you should be using local_sensitive_file.
