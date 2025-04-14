e38a93b274b99268040c06913fcf756fa4d607d5
praveenshrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-04-14 11:47:15 +0530

    Create day06.tf

---
+++ day06.tf
@@ -0,0 +1,5 @@
+// Day 06: How to destroy the resources created by terraform and how to destroy specific resource created by terraform. 
+
+terraform destroy
+  
+terraform destroy -target aws_instance.my_EC2_instance
